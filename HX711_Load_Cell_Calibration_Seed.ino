0eba4906aef5787e1e5d767c90dde500611a5424
Ralph Bacon <arduino@jargongeneration.uk>
2018-01-14 18:05:20 +0000

    Step 1 (Do this first!)

---
+++ HX711_Load_Cell_Calibration_Seed.ino
@@ -0,0 +1,30 @@
+#include "HX711.h"
+#define DOUT  3
+#define CLK  2
+float seed = -2712.95;
+
+HX711 scale(DOUT, CLK);
+
+void setup() {
+  Serial.begin(9600);
+
+  scale.set_scale(seed);
+
+  scale.tare();
+}
+
+void loop() {
+  scale.set_scale(seed);
+
+  if (Serial.available()) {
+    char z = Serial.read();
+
+    if (z == '-') seed -= 5;
+    if (z == '+') seed += 5;
+  }
+
+  Serial.print("Seed value: ");
+  Serial.print(seed);
+  Serial.print("\tWeight:\t");
+  Serial.println(scale.get_units(10), 1);
+}
